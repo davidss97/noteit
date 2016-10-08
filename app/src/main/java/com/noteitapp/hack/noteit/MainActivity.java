78497ea12444343f3c82e7bf0c80e7a5945c07a6
davidss97 <davidsanchezsoles@gmail.com>
2016-10-08 10:41:15 +0200

    Independent fragment tabs

--- app/src/main/java/com/noteitapp/hack/noteit/MainActivity.java
+++ app/src/main/java/com/noteitapp/hack/noteit/MainActivity.java
@@ -1,108 +1,98 @@
+
 package com.noteitapp.hack.noteit;
 
-import android.app.Activity;
-import android.content.Context;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+        import android.app.Activity;
+        import android.content.Context;
+        import android.os.Bundle;
+        import android.support.v4.app.Fragment;
+        import android.support.v4.view.ViewPager;
+        import android.support.v7.widget.Toolbar;
+        import android.view.KeyEvent;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.view.inputmethod.EditorInfo;
+        import android.view.inputmethod.InputMethodManager;
+        import android.widget.ArrayAdapter;
+        import android.widget.AutoCompleteTextView;
+        import android.widget.Button;
+        import android.widget.TextView;
+        import android.widget.Toast;
 
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
-import android.view.inputmethod.InputMethodManager;
-import android.widget.TextView;
+public class MainActivity extends Activity {
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+    private static final String ARG_SECTION_NUMBER = "section_number";
 
-public class MainActivity extends AppCompatActivity {
 
     /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     * Returns a new instance of this fragment for the given section
+     * number.
      */
-    private SectionsPagerAdapter mSectionsPagerAdapter;
 
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    private ViewPager mViewPager;
 
-    int testvar = 0;
+    Button find;
+    AutoCompleteTextView autoComplete;
+
+    String[] CLAUS = new String[]{
+            "AA", "AB", "BA", "BB"
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_enter_code);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
+        find = (Button) findViewById(R.id.button2);
+        final TextView textView = (TextView) findViewById(R.id.section_label);
 
-        //First fragment to show up is the second
-        mViewPager.setCurrentItem(1);
 
+        autoComplete = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView2);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getBaseContext(),
+                android.R.layout.simple_dropdown_item_1line, CLAUS);
+        autoComplete.setThreshold(1);
+        autoComplete.setAdapter(adapter);
+
+        find.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                botopremut();
+            }
+        });
+
+        autoComplete.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN){
+                    botopremut();
+                    return true;
+                }
+                return false;
+            }
+        });
 
     }
 
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            if (position == 0){
-                return EnterCodeFragment.newInstance(position+1);
-            }else if (position == 1){
-                return CameraImage.newInstance(position+1);
-            }
-            else{
-                return PlaceholderFragment.newInstance(position+1);
+    public void botopremut() {
+        String codi = autoComplete.getText().toString();
+        int esta = 0;
+        for (int i = 0; i < CLAUS.length; i++) {
+            if (codi.equals(CLAUS[i])) {
+                esta = 1;
+                Toast toastWIN = Toast.makeText(this.getBaseContext(), codi, 5);
+                toastWIN.show();
+                break;
             }
         }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "SECTION 1";
-                case 1:
-                    return "SECTION 2";
-                case 2:
-                    return "SECTION 3";
-            }
-            return null;
+        if (esta == 0) {
+            Toast toastLOOSE = Toast.makeText(this.getBaseContext(), "Image not found. Please enter a valid key", 7);
+            toastLOOSE.show();
         }
     }
 }
